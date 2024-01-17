224be3d47f8cfda1410c427aa4fb3987beed92ec
Kimo <JamesKimoGreen@gmail.com>
2024-01-16 19:37:59 -0800

    prep assinment 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        String playAgain = "yes";
+        while (playAgain == "yes") 
+        {
+            Boolean guessedCorrect = false;
+            float magicNumber;
+            float guess;
+            Random rnd = new Random();
+            int numberOfGuesses = 0;
+            magicNumber = rnd.Next(1, 101);
+            while (guessedCorrect == false)
+            {    
+                Console.Write("What is your guess? ");
+                guess = Single.Parse(Console.ReadLine());
+                numberOfGuesses = numberOfGuesses + 1;
+                if ( guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                } else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                } else if (guess == magicNumber)
+                {
+                    guessedCorrect = true;
+                }
+            }
+        
+        Console.WriteLine($"You guessed it in {numberOfGuesses}!");
+        Console.WriteLine("Would you like to play again?");
+        playAgain = Console.ReadLine();
+        }
     }
 }
