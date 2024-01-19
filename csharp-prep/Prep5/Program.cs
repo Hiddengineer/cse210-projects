59b6879d250ae93cb455acecfd3fde7debf934e3
Kimo <JamesKimoGreen@gmail.com>
2024-01-18 22:19:25 -0800

    finished prep assinment 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string userName = PromptUserName();
+        float userNumber = PromptUserNumber();
+        float squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static void DisplayResult(string name, float square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static float PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        float number = float.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static float SquareNumber(float number)
+    {
+        
+        float square = number * number;
+        return square;
     }
 }
