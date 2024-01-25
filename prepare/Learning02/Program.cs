e23ca76577763d185877a263591e40b47ad1c1c3
Kimo <JamesKimoGreen@gmail.com>
2024-01-24 22:27:13 -0800

    prepare week 3 assinment all 3 classes done and displaying correctly

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "math tutor";
+        job1._company = "columbiaBasinColege";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "math tutor";
+        job2._company = "BYUI";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        Resume resume1 = new Resume();
+        resume1._name = "James";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
     }
 }
