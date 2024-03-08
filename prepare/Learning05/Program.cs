835cc68837e61cf65caa16b242fe4a425485897f
Kimo <JamesKimoGreen@gmail.com>
2024-03-07 20:08:48 -0800

    prep 5 done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+         List<Shape> shapes = new List<Shape>{
+            new Square("purple", 13),
+            new Rectangle("orange", 10, .5),
+            new Circle("black", 8.4)};
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
